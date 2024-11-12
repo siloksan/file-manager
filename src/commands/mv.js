3ade75365f802e63d21df70f1f7b54d623370ff5
siloksan <tutel07@gmail.com>
2024-11-12 15:32:49 +0300

    feat: create mv command

---
+++ src/commands/mv.js
@@ -0,0 +1,24 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { mkdir, unlink } from 'fs/promises';
+import { join } from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { ERROR_MESSAGES } from '#src/constants/const.js';
+
+export async function mv(args) {
+	if (args.length !== 2) {
+		console.log(ERROR_MESSAGES.invalidInput);
+		return;
+	}
+
+	const [sourcePath, targetDirectory] = args;
+	const fileName = sourcePath.split('/').pop();
+	const destinationPath = join(targetDirectory, fileName);
+
+	try {
+		await mkdir(targetDirectory, { recursive: true });
+		await pipeline(createReadStream(sourcePath), createWriteStream(destinationPath));
+		await unlink(sourcePath);
+	} catch (error) {
+		console.error(`Failed to move file: ${error.message}`);
+	}
+}
