75214e9c53dd4dd54a222815430410c1168d388a
siloksan <tutel07@gmail.com>
2024-11-12 17:21:28 +0300

    feat: create hash commnd

---
+++ src/commands/hash.js
@@ -0,0 +1,23 @@
+import crypto from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { ERROR_MESSAGES } from '#src/constants/const.js';
+
+export async function hash(args) {
+	if (args.length !== 1) {
+		console.log(ERROR_MESSAGES.invalidInput);
+		return;
+	}
+	return new Promise((res, rej) => {
+		const hash = crypto.createHash('sha256');
+		const [pathName] = args;
+		const readStream = createReadStream(pathName);
+
+		readStream.pipe(hash).on('finish', () => {
+			console.log(hash.digest('hex'));
+			res();
+		});
+		readStream.on('error', (err) => {
+			rej(err);
+		});
+	});
+}
